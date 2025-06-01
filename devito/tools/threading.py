eedb65f2dd68b577430a22f4c7bb91b26a58917c
enwask <enwask@ucf.edu>
2025-06-02 00:31:44 +0200

    Add free threading check

--- devito/tools/threading.py
+++ devito/tools/threading.py
@@ -1,6 +1,9 @@
+import sys
 import threading
 
-__all__ = ['sympy_mutex', 'safe_dict_copy']
+from functools import cache
+
+__all__ = ['sympy_mutex', 'safe_dict_copy', 'is_free_threading']
 
 
 sympy_mutex = threading.RLock()
@@ -24,3 +27,26 @@ def safe_dict_copy(v):
             return v.copy()
         except RuntimeError:
             pass
+
+
+def _is_gil_enabled() -> bool:
+    """
+    Checks if the GIL is enabled.
+
+    Python 3.13+ has a build flag to disable the GIL for proper
+    multithreading; we check if the version is high enough and
+    that flag has been set.
+    """
+    if sys.version_info < (3, 13):
+        return True
+
+    return sys._is_gil_enabled()
+
+
+@cache
+def is_free_threading() -> bool:
+    """
+    Checks if the current Python interpreter is free-threading; i.e.,
+    the Global Interpreter Lock (GIL) is disabled.
+    """
+    return not _is_gil_enabled()
